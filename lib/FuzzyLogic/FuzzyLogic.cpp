09bc4e96e9d5db47ae3db9f4089f47e24f3f6db1
Giovane Torres <gdotorres@inf.ufpel.edu.br>
2015-11-05 12:41:11 -0200

    Basic AND & OR operations done, can add more

--- lib/FuzzyLogic/FuzzyLogic.cpp
+++ lib/FuzzyLogic/FuzzyLogic.cpp
@@ -13,3 +13,19 @@ double FuzzyLogic::Not2(double x) {
 double FuzzyLogic::Not3(double x) {
 	return pow(1 - pow(x, 3), 1.0 / 3);
 }
+
+double FuzzyLogic::And(double x, double y){
+	return x < y ? x : y;
+}
+
+double FuzzyLogic::And2(double x, double y){
+	return x*y;
+}
+
+double FuzzyLogic::Or(double x, double y){
+	return x > y ? x : y;
+}
+
+double FuzzyLogic::Or2(double x, double y){
+	return (x+y)-(x*y);
+}
