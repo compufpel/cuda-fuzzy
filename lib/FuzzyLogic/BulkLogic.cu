db31806e68fd6733f035b1bfdf8d0451f3bd5dd4
gppaim <gppaim@inf.ufpel.edu.br>
2015-11-27 12:33:54 -0300

    Fuzzy Logic Documentation

--- lib/FuzzyLogic/BulkLogic.cu
+++ lib/FuzzyLogic/BulkLogic.cu
@@ -8,10 +8,11 @@
 #include "BulkLogic.cuh"
 
 /*___h_BulkNot___
-*   Objective: Implement the sequential version of not operation (1-Input) with Fuzzy array.
+*   Function: Implement the sequential version of not operation (1-Input) with Fuzzy array.
 *   Parameters:
-*              -- double* v: Array Fuzzy that will be operated 
-*              -- int size : Number of array elements
+*   Input: double* v: Array Fuzzy that will be operated 
+*          int size : Number of array elements
+*   Output: double* : Operated Array Fuzzy
 *   Creation date: November, 2015.
 *   Exception case: -
 */
@@ -28,10 +29,11 @@ double* h_BulkNot(double* v, int size) {
 }
 
 /*___h_BulkNot2___
-*   Objective: Implement the sequential version of not operation sqrt(1 - pow(v[n], 2)) with Fuzzy array.
+*   Function: Implement the sequential version of not operation sqrt(1 - pow(v[n], 2)) with Fuzzy array.
 *   Parameters:
-*              -- double* v: Array Fuzzy that will be operated 
-*              -- int size : Number of array elements
+*   Input: double* v: Array Fuzzy that will be operated 
+*          int size : Number of array elements
+*   Output: double* : Operated Array Fuzzy
 *   Creation date: November, 2015.
 *   Exception case: -
 */
@@ -46,10 +48,11 @@ double* h_BulkNot2(double* v, int size) {
 }
 
 /*___h_BulkNot3___
-*   Objective: Implement the sequential version of not operation pow(1 - pow(v[n], 3), 1.0 / 3) with Fuzzy array.
+*   Function: Implement the sequential version of not operation pow(1 - pow(v[n], 3), 1.0 / 3) with Fuzzy array.
 *   Parameters:
-*              -- double* v: Array Fuzzy that will be operated 
-*              -- int size : Number of array elements
+*   Input: double* v: Array Fuzzy that will be operated 
+*          int size : Number of array elements
+*   Output: double* : Operated Array Fuzzy
 *   Creation date: November, 2015.
 *   Exception case: -
 */
@@ -64,10 +67,11 @@ double* h_BulkNot3(double* v, int size) {
 }
 
 /*___h_BulkAnd___
-*   Objective: Implement the sequential version of and operation (<) with Fuzzy array.
+*   Function: Implement the sequential version of and operation (<) with Fuzzy array.
 *   Parameters:
-*              -- double* v: Array Fuzzy that will be operated 
-*              -- int size : Number of array elements
+*   Input: double* v: Array Fuzzy that will be operated 
+*          int size : Number of array elements
+*   Output: double* : Operated Array Fuzzy
 *   Creation date: November, 2015.
 *   Exception case: -
 */
@@ -83,10 +87,11 @@ double* h_BulkAnd(double* v, double* w, int size) {
 }
 
 /*___h_BulkAnd2___
-*   Objective: Implement the sequential version of and operation (*) with Fuzzy array.
+*   Function: Implement the sequential version of and operation (*) with Fuzzy array.
 *   Parameters:
-*              -- double* v: Array Fuzzy that will be operated 
-*              -- int size : Number of array elements
+*   Input: double* v: Array Fuzzy that will be operated 
+*          int size : Number of array elements
+*   Output: double* : Operated Array Fuzzy
 *   Creation date: November, 2015.
 *   Exception case: -
 */
@@ -101,10 +106,11 @@ double* h_BulkAnd2(double* v, double* w, int size) {
 }
 
 /*___h_BulkOr___
-*   Objective: Implement the sequential version of not operation (>) with Fuzzy array.
+*   Function: Implement the sequential version of not operation (>) with Fuzzy array.
 *   Parameters:
-*              -- double* v: Array Fuzzy that will be operated 
-*              -- int size : Number of array elements
+*   Input: double* v: Array Fuzzy that will be operated 
+*          int size : Number of array elements
+*   Output: double* : Operated Array Fuzzy
 *   Creation date: November, 2015.
 *   Exception case: -
 */			
@@ -119,10 +125,11 @@ double* h_BulkOr(double* v, double* w, int size) {
 }
 
 /*___h_BulkOr2___
-*   Objective: Implement the sequential version of not operation (( v[n] + w[n] ) - ( v[n] * w[n] )) with Fuzzy array.
+*   Function: Implement the sequential version of not operation (( v[n] + w[n] ) - ( v[n] * w[n] )) with Fuzzy array.
 *   Parameters:
-*              -- double* v: Array Fuzzy that will be operated 
-*              -- int size : Number of array elements
+*   Input: double* v: Array Fuzzy that will be operated 
+*          int size : Number of array elements
+*   Output: double* : Operated Array Fuzzy
 *   Creation date: November, 2015.
 *   Exception case: -
 */
--- lib/FuzzyLogic/FuzzyLogic.cu
+++ lib/FuzzyLogic/FuzzyLogic.cu
@@ -1,29 +1,92 @@
+/* CudaFuzzy Project - 2015 
+ * Graduate Program in Computer Science - UFPel
+ *
+ * \file FuzzyLogic.cu
+ *      This file contains the sequential implementations of fuzzy functions applied to arrays.
+ */
+
 #include "FuzzyLogic.cuh"
 
+/*___Not___
+*   Function: Implement the sequential version of not operation (1-Input) with Fuzzy array.
+*   Parameters:
+*   Input:  double* x: Input of Basic Fuzzy operation
+*   Output: double   : Response of Fuzzy operation 
+*   Creation date: November, 2015.
+*   Exception case: -
+*/
 double Not(double x) {
 	return 1 - x;
 }
 
+/*___Not2___
+*   Function: Implement the sequential version of not operation sqrt(1 - pow(x, 2)) with Fuzzy array.
+*   Parameters:
+*   Input:  double* x: Input of Basic Fuzzy operation
+*   Output: double   : Response of Fuzzy operation 
+*   Creation date: November, 2015.
+*   Exception case: -
+*/
 double Not2(double x) {
 	return sqrt(1 - pow(x, 2));
 }
 
+/*___Not3___
+*   Function: Implement the sequential version of not operation pow(1 - pow(x, 3), 1.0 / 3) with Fuzzy array.
+*   Parameters:
+*   Input:  double* x: Input of Basic Fuzzy operation
+*   Output: double   : Response of Fuzzy operation 
+*   Creation date: November, 2015.
+*   Exception case: -
+*/
 double Not3(double x) {
 	return pow(1 - pow(x, 3), 1.0 / 3);
 }
 
+/*___And___
+*   Function: Implement the sequential version of and operation (<) with Fuzzy array.
+*   Parameters:
+*   Input:  double* x: Input of Basic Fuzzy operation
+*   Output: double   : Response of Fuzzy operation 
+*   Creation date: November, 2015.
+*   Exception case: -
+*/
 double And(double x, double y) {
 	return x < y ? x : y;
 }
 
+/*___And2___
+*   Function: Implement the sequential version of and operation (*) with Fuzzy array.
+*   Parameters:
+*   Input:  double* x: Input of Basic Fuzzy operation
+*   Output: double   : Response of Fuzzy operation 
+*   Creation date: November, 2015.
+*   Exception case: -
+*/
 double And2(double x, double y) {
 	return x * y;
 }
 
+/*___Or___
+*   Function: Implement the sequential version of not operation (>) with Fuzzy array.
+*   Parameters:
+*   Input:  double* x: Input of Basic Fuzzy operation
+*   Output: double   : Response of Fuzzy operation 
+*   Creation date: November, 2015.
+*   Exception case: -
+*/
 double Or(double x, double y) {
 	return x > y ? x : y;
 }
 
+/*___h_BulkOr2___
+*   Function: Implement the sequential version of not operation (( x + y ) - ( x * y )) with Fuzzy array.
+*   Parameters:
+*   Input:  double* x: Input of Basic Fuzzy operation
+*   Output: double   : Response of Fuzzy operation 
+*   Creation date: November, 2015.
+*   Exception case: -
+*/
 double Or2(double x, double y) {
 	return ( x + y ) - ( x * y );
 }
