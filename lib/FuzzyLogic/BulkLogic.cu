1dd133fc5094f890460134919479b17e1b0bbdee
gppaim <gppaim@inf.ufpel.edu.br>
2015-11-27 10:40:34 -0300

    Documentation - Sequential Bulk Logic

--- lib/FuzzyLogic/BulkLogic.cu
+++ lib/FuzzyLogic/BulkLogic.cu
@@ -1,3 +1,10 @@
+/* CudaFuzzy Project - 2015 
+ * Graduate Program in Computer Science - UFPel
+ *
+ * \file BulkLogic.cu
+ *      This file contains the sequential implementations of fuzzy functions applied to arrays.
+ */
+
 #include "BulkLogic.cuh"
 
 /*___h_BulkNot___
@@ -20,7 +27,14 @@ double* h_BulkNot(double* v, int size) {
 	return result;
 }
 
-
+/*___h_BulkNot2___
+*   Objective: Implement the sequential version of not operation sqrt(1 - pow(v[n], 2)) with Fuzzy array.
+*   Parameters:
+*              -- double* v: Array Fuzzy that will be operated 
+*              -- int size : Number of array elements
+*   Creation date: November, 2015.
+*   Exception case: -
+*/
 double* h_BulkNot2(double* v, int size) {
 	double* result = (double*)malloc(sizeof(double) * size);
 
@@ -31,6 +45,14 @@ double* h_BulkNot2(double* v, int size) {
 	return result;
 }
 
+/*___h_BulkNot3___
+*   Objective: Implement the sequential version of not operation pow(1 - pow(v[n], 3), 1.0 / 3) with Fuzzy array.
+*   Parameters:
+*              -- double* v: Array Fuzzy that will be operated 
+*              -- int size : Number of array elements
+*   Creation date: November, 2015.
+*   Exception case: -
+*/
 double* h_BulkNot3(double* v, int size) {
 	double* result = (double*)malloc(sizeof(double) * size);
 
@@ -41,6 +63,14 @@ double* h_BulkNot3(double* v, int size) {
 	return result;
 }
 
+/*___h_BulkAnd___
+*   Objective: Implement the sequential version of and operation (<) with Fuzzy array.
+*   Parameters:
+*              -- double* v: Array Fuzzy that will be operated 
+*              -- int size : Number of array elements
+*   Creation date: November, 2015.
+*   Exception case: -
+*/
 double* h_BulkAnd(double* v, double* w, int size) {
 	
 	double* result = (double*)malloc(sizeof(double) * size);
@@ -52,6 +82,14 @@ double* h_BulkAnd(double* v, double* w, int size) {
 	return result;
 }
 
+/*___h_BulkAnd2___
+*   Objective: Implement the sequential version of and operation (*) with Fuzzy array.
+*   Parameters:
+*              -- double* v: Array Fuzzy that will be operated 
+*              -- int size : Number of array elements
+*   Creation date: November, 2015.
+*   Exception case: -
+*/
 double* h_BulkAnd2(double* v, double* w, int size) {
 	double* result = (double*)malloc(sizeof(double) * size);
 
@@ -61,7 +99,15 @@ double* h_BulkAnd2(double* v, double* w, int size) {
 
 	return result;
 }
-			
+
+/*___h_BulkOr___
+*   Objective: Implement the sequential version of not operation (>) with Fuzzy array.
+*   Parameters:
+*              -- double* v: Array Fuzzy that will be operated 
+*              -- int size : Number of array elements
+*   Creation date: November, 2015.
+*   Exception case: -
+*/			
 double* h_BulkOr(double* v, double* w, int size) {
 	double* result = (double*)malloc(sizeof(double) * size);
 
@@ -72,6 +118,14 @@ double* h_BulkOr(double* v, double* w, int size) {
 	return result;
 }
 
+/*___h_BulkOr2___
+*   Objective: Implement the sequential version of not operation (( v[n] + w[n] ) - ( v[n] * w[n] )) with Fuzzy array.
+*   Parameters:
+*              -- double* v: Array Fuzzy that will be operated 
+*              -- int size : Number of array elements
+*   Creation date: November, 2015.
+*   Exception case: -
+*/
 double* h_BulkOr2(double* v, double* w, int size) {
 	double* result = (double*)malloc(sizeof(double) * size);
 
